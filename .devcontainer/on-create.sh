586644905c1ea9bd1d6b5f0ac77177df45fdc36f
bartr <bartr@microsoft.com>
2021-08-26 09:48:21 +0000

    updated setup

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -21,4 +21,29 @@ docker network create k3d
 k3d registry create registry.localhost --port 5000
 docker network connect k3d k3d-registry.localhost
 
+# install postgres
+docker run -d \
+--name postgres \
+--restart always \
+-e POSTGRES_PASSWORD=akdc-512 \
+-v /ago/postgres/data:/var/lib/postgresql/data \
+--network k3d \
+-p 5432:5432 \
+postgres
+
+sleep 20
+
+# create git db and user
+docker exec -it postgres psql -U postgres -c "create user gogs with password 'akdc-512';"
+docker exec -it postgres psql -U postgres -c "create database gogs owner gogs;"
+
+# run local git instance
+docker run -d \
+--name gogs \
+--restart always \
+--network k3d \
+-p 3000:3000 -p 8322:22 \
+-v /ago/gogs/data:/data \
+gogs/gogs
+
 echo "on-create complete" >> ~/status
