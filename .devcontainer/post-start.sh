b2fe4327675f7a96b220b33b45bc19be47b2cbe0
bartr <bartr@microsoft.com>
2021-09-07 00:09:06 +0000

    updated build

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -8,5 +8,8 @@ echo "post-start start" >> ~/status
 docker pull mcr.microsoft.com/dotnet/aspnet:5.0-alpine
 docker pull mcr.microsoft.com/dotnet/sdk:5.0
 docker pull ghcr.io/cse-labs/jumpbox:latest
+docker pull postgres:latest
+docker pull registry:2
+docker pull gogs/gogs:latest
 
 echo "post-start complete" >> ~/status
